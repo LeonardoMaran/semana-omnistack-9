378125a42693903daea4b288df2f53bbe031f177
Leonardo <leonardogmaran9@gmail.com>
2019-11-28 15:25:54 -0300

    aircnc backend

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,1 +1,31 @@
+const User = require('../models/User');
+
 //00.44.05
+//Sessao, pode ser login, logout, qualquer coisa relacionada com sessao
+
+//dentro do controller temos os seguntes metodos
+//index, show, store, update, e destroi
+
+//metodo index: esa demonstrando que agente vai retornar um metod de listagem de sessoes
+//o show ele vai listar uma unica sessao
+//o store e quando eu quero criar uma sessao
+//o update e quando eu quero alterar uma sessao
+//e o destroi e quando eu quero remover, deletar uma sessao
+
+//estes ao os metodos disponives que eu posso ter em um controller
+// posso criar mais metodos, posso, mas por padrao da comunidade eu nao posso criar mais metodos
+// que este controller. Se voceprecisa criar mais metodos que este aqui provavelmente voce precia estar
+//criando outro controller
+
+//como eu quero cirar uma sessao eu vou utilizar o metodo store
+
+module.exports = {
+    async store(req, res){
+            const { email } = req.body;
+
+            const user = await User.create({ email });
+
+            return res.json(user);
+        }
+    };
+
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 //router: roteador do express, responsavel peelas rotas
 
-routes.post('/', (req, res)=>{
-    return res.json(req.body);
-});
+routes.post('/users', SessionController.store);
+
+//50.39
 
 module.exports = routes;
